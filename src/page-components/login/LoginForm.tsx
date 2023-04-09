74e93da4ba1595a313ab4b2d8cbd7a911a4b9845
plasticine <975036719@qq.com>
2023-04-09 22:04:01 +0800

    feat(login): 完成 LoginForm UI

--- src/page-components/login/LoginForm.tsx
+++ src/page-components/login/LoginForm.tsx
@@ -1,26 +1,70 @@
-import { Card, CardContent, CardHeader, Input, Stack } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material'
+
+import { useBoolean } from '@/hooks'
 
 export const LoginForm: React.FC = () => {
+  const [passwordHidden, , togglePasswordHidden] = useBoolean(true)
+
   return (
-    <Card className="use-bg-layer max-w-md">
-      <CardHeader>
-        <Stack direction="column" spacing={4}>
-          <h2>Plasticine Admin</h2>
-          <Stack direction="column" spacing={1}>
-            <h3>Welcome to Plasticine Admin!👋🏻</h3>
-            <p className="text-gray-400 dark:text-gray-300 text-xs">
+    <Card className="max-w-md p-4">
+      <CardHeader
+        title={
+          <Typography variant="h5" textAlign={'center'}>
+            Plasticine Admin
+          </Typography>
+        }
+        subheader={
+          <Stack className="mt-4">
+            <Typography variant="h6">Welcome to Plasticine Admin!👋🏻</Typography>
+            <Typography variant="body2" color="GrayText">
               Please sign-in to your account and start the adventure
-            </p>
+            </Typography>
           </Stack>
-        </Stack>
-      </CardHeader>
+        }
+      />
 
       <CardContent>
-        <Stack>
-          <Input className="input-primary" placeholder="Username" />
-          <Input className="input-primary" placeholder="Password" />
+        <Stack spacing={2}>
+          <TextField label="Username" />
+
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <OutlinedInput
+              id="password"
+              label="Password"
+              type={passwordHidden ? 'password' : 'text'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton onClick={togglePasswordHidden} edge="end">
+                    {passwordHidden ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
         </Stack>
       </CardContent>
+
+      <CardActions className='px-4'>
+        <Button className="w-full" variant="contained">
+          Sign In
+        </Button>
+      </CardActions>
     </Card>
   )
 }
